b98286b36870d8c5c72d94ea3360c4d3143d67ad
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-11-07 19:56:39 -0600

    post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from chalice import Chalice
+from chalice.app import Chalice
 
 app = Chalice(app_name='rest-api-with-python')
 
@@ -8,22 +8,8 @@ def index():
     return {'hello': 'world'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/post', methods=['POST'])
+def create_post():
+    request = app.current_request
+    message = request.json_body
+    return message
